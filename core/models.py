2ddc3809c1326a8faba7d6b3427b5ed838c4cc44
AlisaLC <ali_sal1381@yahoo.com>
2023-10-18 18:33:34 +0330

    fixed group description

--- core/models.py
+++ core/models.py
@@ -36,7 +36,7 @@ class Challenger(models.Model):
 
 class Group(models.Model):
     name = models.CharField(max_length=50)
-    description = models.TextField(max_length=500)
+    description = models.TextField(max_length=500, blank=True)
 
     def __str__(self):
         return f'{self.name}'
